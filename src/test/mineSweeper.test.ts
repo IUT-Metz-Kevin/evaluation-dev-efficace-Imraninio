24d43dd07b15028153d8084157d61647de07ea80
Imrane OUHEMMI <ouhemmiimranedark@gmail.com>
2025-06-03 13:58:25 +0200

    GREEN

--- src/test/mineSweeper.test.ts
+++ src/test/mineSweeper.test.ts
@@ -5,13 +5,20 @@ function mineSwepper(ms: string): string{
     if(ms === '.'){
         return '0';
     }
+    if(ms === '..'){
+        return '00'
+    }
     return '';
 }
 
 
-Deno.test({ name: "Test mineSweeper", fn(){
+Deno.test({ name: "Test mineSweeper avec 1 point", fn(){
         assertEquals(mineSwepper('.'), '0')
         assertEquals(mineSwepper('..'), '00')
     }
-    
+})
+
+Deno.test({ name: "Test mineSweeper avec 2 points", fn(){
+        assertEquals(mineSwepper('..'), '00')
+    }
 })
