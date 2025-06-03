d6e3dd3fb8491781c976e699e1be50c1bfdd4af0
Imrane OUHEMMI <ouhemmiimranedark@gmail.com>
2025-06-03 16:13:38 +0200

    REFACTOR

--- src/test/mineSweeper.test.ts
+++ src/test/mineSweeper.test.ts
@@ -1,35 +1,42 @@
 import { assertEquals } from "jsr:@std/assert";
 
 function mineSwepper(ms: string): string{
+    // tableau de chaine où chaque chaine est une ligne du mineSwepper
     const tabMineSwepper = ms.split('\n');
     let mineSweeperStr = "";
+    // boucle sur chaque ligne
     for(let i = 0; i < tabMineSwepper.length; i++){
+        // boucle sur chaque index de la ligne actuelle
         for(let j = 0; j < tabMineSwepper[i].length + 1; j++){                          
             let nbBombesAutour = 0;
             if(tabMineSwepper[i][j] === '.'){
                 for(let k = -1; k < 2; k++){
+                    // ligne actuelle
                     if(tabMineSwepper[i][j+k] === "*"){
                         nbBombesAutour++;
                     }
-
+                    // ligne du dessous
                     if(i<tabMineSwepper.length-1){
                         if(tabMineSwepper[i+1][j+k] === "*"){
                             nbBombesAutour++;
                         }
                     }
-
+                    // ligne du dessus
                     if(i>0){
                         if(tabMineSwepper[i-1][j+k] === "*"){
                             nbBombesAutour++;
                         }
                     }
                 }
+                // ajout du nombre de bombes autour
                 mineSweeperStr+= nbBombesAutour.toString()
             }
+            // ajout de la bombe
             else if(tabMineSwepper[i][j] === '*'){
                 mineSweeperStr += '*'
             }
         }
+        // séparer chaque ligne
         if(tabMineSwepper.length > 1 && tabMineSwepper.length-1 > i){
             mineSweeperStr += '\n';
         }
@@ -89,7 +96,7 @@ Deno.test({ name: "Test mineSweeper avec 3 bombes", fn(){
 })
  
 Deno.test({ name: "Test mineSweeper avec plusieurs lignes et plusieurs bombes", fn(){
-        assertEquals(mineSwepper(".*.**.\n...*.\n..*..."), "1*2**2\n1234*2\n01*211")
+        assertEquals(mineSwepper(".*.**.\n....*.\n..*..."), "1*2**2\n1234*2\n01*211")
     }
 })
 
