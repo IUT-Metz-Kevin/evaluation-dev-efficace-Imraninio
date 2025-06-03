eb4c9cc3005248929e8fff970b0816719559a9ba
Imrane OUHEMMI <ouhemmiimranedark@gmail.com>
2025-06-03 13:50:45 +0200

    RED

--- src/test/mineSweeper.test.ts
+++ src/test/mineSweeper.test.ts
@@ -6,7 +6,7 @@ import { assertEquals } from "jsr:@std/assert";
 
 Deno.test({ name: "test simple avec une ligne",
     fn(){
-        assertEquals('......', '000000')
+        assertEquals('.', '0')
     }
     
 })
