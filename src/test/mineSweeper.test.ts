0a45978c19feee3c50abe3293b2759250969ab1b
Imrane OUHEMMI <ouhemmiimranedark@gmail.com>
2025-06-03 13:52:56 +0200

    GREEN

--- src/test/mineSweeper.test.ts
+++ src/test/mineSweeper.test.ts
@@ -1,12 +1,16 @@
 import { assertEquals } from "jsr:@std/assert";
 
 
+function mineSwepper(ms: string): string{
+    if(ms === '.'){
+        return '0';
+    }
+    return '';
+}
 
 
-
-Deno.test({ name: "test simple avec une ligne",
-    fn(){
-        assertEquals('.', '0')
+Deno.test({ name: "test simple avec une ligne", fn(){
+        assertEquals(mineSwepper('.'), '0')
     }
     
 })
